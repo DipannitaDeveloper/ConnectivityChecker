60f96d86a60b3517c40e4486ea13f877928b155b
DipannitaDeveloper <dipannita.git@gmail.com>
2020-03-01 19:10:44 +0530

    initial commit

---
+++ internetstatus/src/main/java/com/dipannita/internetstatus/ConnectivityStatus.java
@@ -0,0 +1,39 @@
+package com.dipannita.internetstatus;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+
+public class ConnectivityStatus {
+
+    private static ConnectivityStatus instance = new ConnectivityStatus();
+    static Context context;
+    ConnectivityManager connectivityManager;
+    NetworkInfo wifiInfo, mobileInfo;
+    boolean connected = false;
+
+    public static ConnectivityStatus getInstance(Context ctx) {
+        context = ctx.getApplicationContext();
+        return instance;
+    }
+
+    public boolean isOnline() {
+        try {
+            connectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            connected = networkInfo != null && networkInfo.isAvailable() &&
+                    networkInfo.isConnected();
+            return connected;
+
+
+        } catch (Exception e) {
+            System.out.println("CheckConnectivity Exception: " + e.getMessage());
+            Log.v("connectivity", e.toString());
+        }
+        return connected;
+    }
+}
+
